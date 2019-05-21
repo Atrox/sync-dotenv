469c76783d37df84344ac5f1d01df0fcf986f8e5
Atrox <hello@atrox.dev>
2019-05-21 16:49:59 +0200

    improve tests

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
@@ -40,25 +41,77 @@ KEY3=EXAMPLEVALUE
 KEY4=
 `
 
-func init() {
-	fs = afero.NewMemMapFs()
+const dotEnvFileWithChanges = `
+# COMMENT 2
+KEY3=VALUE3
 
+# COMMENT 1
+KEY1=VALUE1
+KEY2="VALUE2"
+`
+
+const dotEnvExpectedWithChanges = `
+# COMMENT 2
+KEY3=EXAMPLEVALUE
+
+# COMMENT 1
+KEY1=
+KEY2=
+`
+
+func init() {
 	envFilePath = ".env"
 	exampleFilePath = ".env.example"
 }
 
-func Test_processFile(t *testing.T) {
+func TestRun(t *testing.T) {
+	fs = afero.NewMemMapFs()
+
 	err := afero.WriteFile(fs, envFilePath, []byte(dotEnvFile), os.ModePerm)
 	assert.Nil(t, err)
 
 	err = afero.WriteFile(fs, exampleFilePath, []byte(dotEnvExampleFile), os.ModePerm)
 	assert.Nil(t, err)
 
-	err = startSync()
+	err = run(nil, nil)
 	assert.Nil(t, err)
 
 	fileContent, err := afero.ReadFile(fs, exampleFilePath)
 	assert.Nil(t, err)
 
 	assert.Equal(t, dotEnvExpected, string(fileContent), "they should be equal")
+}
+
+func TestWatch(t *testing.T) {
+	fs = afero.NewOsFs()
+	watch = true
+
+	err := afero.WriteFile(fs, envFilePath, []byte(dotEnvFile), os.ModePerm)
+	assert.Nil(t, err)
+
+	err = afero.WriteFile(fs, exampleFilePath, []byte(dotEnvExampleFile), os.ModePerm)
+	assert.Nil(t, err)
+
+	go func() {
+		err = run(nil, nil)
+	}()
+
+	time.Sleep(100 * time.Millisecond)
+
+	err = afero.WriteFile(fs, envFilePath, []byte(dotEnvFileWithChanges), os.ModePerm)
+	assert.Nil(t, err)
+
+	time.Sleep(100 * time.Millisecond)
+
+	fileContent, err := afero.ReadFile(fs, exampleFilePath)
+	assert.Nil(t, err)
+
+	assert.Equal(t, dotEnvExpectedWithChanges, string(fileContent), "they should be equal")
+}
+
+func TestWithoutEnvFile(t *testing.T) {
+	fs = afero.NewMemMapFs()
+
+	err := run(nil, nil)
+	assert.NotNil(t, err)
 }
