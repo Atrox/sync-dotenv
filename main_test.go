2fb57a0c4a70fadde0490858934ddaed0c7c3fc9
Atrox <hello@atrox.dev>
2019-05-22 09:36:17 +0200

    simplify tests

--- main_test.go
+++ main_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"os"
 	"testing"
-	"time"
 
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
@@ -41,24 +40,6 @@ KEY3=EXAMPLEVALUE
 KEY4=
 `
 
-const dotEnvFileWithChanges = `
-# COMMENT 2
-KEY3=VALUE3
-
-# COMMENT 1
-KEY1=VALUE1
-KEY2="VALUE2"
-`
-
-const dotEnvExpectedWithChanges = `
-# COMMENT 2
-KEY3=EXAMPLEVALUE
-
-# COMMENT 1
-KEY1=
-KEY2=
-`
-
 func init() {
 	envFilePath = ".env"
 	exampleFilePath = ".env.example"
@@ -80,33 +61,6 @@ func TestRun(t *testing.T) {
 	assert.Nil(t, err)
 
 	assert.Equal(t, dotEnvExpected, string(fileContent), "they should be equal")
-}
-
-func TestWatch(t *testing.T) {
-	fs = afero.NewOsFs()
-	watch = true
-
-	err := afero.WriteFile(fs, envFilePath, []byte(dotEnvFile), os.ModePerm)
-	assert.Nil(t, err)
-
-	err = afero.WriteFile(fs, exampleFilePath, []byte(dotEnvExampleFile), os.ModePerm)
-	assert.Nil(t, err)
-
-	go func() {
-		err = run(nil, nil)
-	}()
-
-	time.Sleep(100 * time.Millisecond)
-
-	err = afero.WriteFile(fs, envFilePath, []byte(dotEnvFileWithChanges), os.ModePerm)
-	assert.Nil(t, err)
-
-	time.Sleep(100 * time.Millisecond)
-
-	fileContent, err := afero.ReadFile(fs, exampleFilePath)
-	assert.Nil(t, err)
-
-	assert.Equal(t, dotEnvExpectedWithChanges, string(fileContent), "they should be equal")
 }
 
 func TestWithoutEnvFile(t *testing.T) {
